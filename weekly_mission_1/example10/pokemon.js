aea0e2504a9f0f56df819135f0e491116afb78ca
arrenchu <oswaldo_arrenchu@hotmail.com>
2022-04-12 18:17:26 -0500

    agrego tarea

---
+++ weekly_mission_1/example10/pokemon.js
@@ -0,0 +1,22 @@
+export default class pokemon {
+    constructor(name) {
+      // this es una variable para referenciar el valor del contexto local de esta clase
+      this.name = name // Estás variables se le conocen como atributos
+    }
+  
+    // método
+    // this.name es la variable que se guarda en el contexto local
+    // message es una variable que se le pasa al ejecutar este método
+
+   // exports.info = (message) => {
+   //     console.log(`info: ${message}`)
+  //    }
+
+    sayHello() {
+      console.log(`mi pokemon ${this.name} te saluda`)
+    }
+  
+    sayMessage(message) {
+        console.log(`mi pokemon ${this.name} dice ${message}`)
+    }
+  }
